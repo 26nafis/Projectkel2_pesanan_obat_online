cf3031b5b3cca91ad9d2ed5c5866724e06bf2fb3
26nafis <spalanzaninafis@gmail.com>
2026-04-14 01:04:54 +0700

    button logout

--- Projectkel2_pesanan_obat_online/FormAdmin.cs
+++ Projectkel2_pesanan_obat_online/FormAdmin.cs
@@ -36,3 +36,12 @@ namespace Projectkel2_pesanan_obat_online
             FormObat f = new FormObat();
             f.Show();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 login = new Form1();
+            login.Show();
+            this.Hide();
+        }
+    }
+}
