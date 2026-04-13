83d2f65f8f77831d580fea752339ada21a6b87e5
26nafis <spalanzaninafis@gmail.com>
2026-04-14 01:10:43 +0700

    konstruktor default

---
+++ Projectkel2_pesanan_obat_online/FormCustomer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Projectkel2_pesanan_obat_online
+{
+    public partial class FormCustomer : Form
+    {
+        SqlConnection conn;
+        string namaUser;
+
+        public FormCustomer()
+        {
+            InitializeComponent();
+
+            conn = new SqlConnection("Data Source=NAFIS\\NAFISCOY;Initial Catalog=ApotekDB;Integrated Security=True");
+        }
+
